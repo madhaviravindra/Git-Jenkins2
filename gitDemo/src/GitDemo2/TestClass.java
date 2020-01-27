b6b39bd535c08a4046ce2ea41a0034f7d1b3473d
ravindrak <ravindrak@COL011018050.infics.com>
2020-01-28 03:32:36 +0530

    firsssst commit

---
+++ gitDemo/src/GitDemo2/TestClass.java
@@ -0,0 +1,12 @@
+package GitDemo2;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	@Test
+	public void SampleTest()
+	{
+		System.out.println("code running on jenkins from Github");
+	}
+
+}
